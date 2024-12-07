320061bb824a0941ff94d7a34cc52c18af6facaa
Wen Ow <ow.wen2022@gmail.com>
2024-12-07 15:03:01 -0500

    saving progress made to clear

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 /*-------------------------------- Constants --------------------------------*/
 
-// Constants would store unchanging values, but this app doesn't need any for now.
+// Constants would store unchanging values
 
 /*-------------------------------- Variables --------------------------------*/
 
+// Use 'let' to manage states and values
 let shouldResetDisplay = false; // Variable to determine if the display should be reset
 let firstNumber = ""; // Variable to store the first number
 let operator = ""; // Variable to store the operator
@@ -11,6 +12,7 @@ let secondNumber = ""; // Variable to store the second number
 
 /*------------------------ Cached Element References ------------------------*/
 
+// 'Const' to save constant things which can't be changed and used multiple times
 const display = document.querySelector(".display"); // Selects the display area element
 const numberButtons = document.querySelectorAll(".number"); // Selects all elements with the class of number (0-9)
 const operatorButtons = document.querySelectorAll(".operator"); // Selects all elements with the class of operator (+, -, *, /, C)
@@ -21,6 +23,8 @@ const clearButton = document.querySelector(".clear"); // Selects the clear "C" b
 
 // Add event listeners to all number buttons
 // When a number button is clicked, its text (the number) is passed to appendNumber
+// appendNumber means to add something to the end of an existing sequence. If there's a number on the display already, it adds the new number to the end of it (concatenates it).
+// If the display is 0, it replaces it with the number.
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => appendNumber(button.textContent));
 });
@@ -33,17 +37,25 @@ operatorButtons.forEach((button) => {
 
 // Add event listener to the equals button
 // When the equals button is clicked, the evaluate function is called
+// You use forEach() to iterate through a list, this is a list of one item, so you don't need to use it here
 equalsButton.addEventListener("click", evaluate);
 
 // Add event listener to the clear button
 // When the clear button is clicked, the clear function is called and the display is reset
+// You use forEach() when you have multiple elements and you want to attach the same behavior to all of them; for the clear button, there is only one, so you don't need to use it here
 clearButton.addEventListener("click", clear);
 
 /*-------------------------------- Functions --------------------------------*/
 
+// Functions are reusable blocks of code that accept inputs, process those inputs, and return a new data value
+// Functions are defined using the function keyword, followed by a name, followed by parentheses ()
+// The code to be executed by the function is placed inside curly brackets {}
+// Functions are called by using the function name followed by parentheses ()
+// Functions can accept parameters, which are values passed to the function when it is called
 function appendNumber(number) {
-  if (shouldResetDisplay) resetDisplay(); // If the display should be reset, call the resetDisplay function
-  display.textContent = display.textContent === "0" ? number : display.textContent + number; // If the display is 0, replace it with the number. Otherwise, append the number
+  if (shouldResetDisplay) resetDisplay(); // If the display should be reset, call the resetDisplay function to clear the display
+  display.textContent =
+    display.textContent === "0" ? number : display.textContent + number; // If the display is 0, replace it with the number. Otherwise, append the number
 }
 
 function resetDisplay() {
@@ -59,6 +71,9 @@ function setOperator(selectedOperator) {
   operator = selectedOperator; // Store the selected operator as the operator
   display.textContent = `${firstNumber} ${operator}`; // Update the display to show the first number and operator in progress
   shouldResetDisplay = true; // Prepare the display for the second number
+
+  // If the selected operator is "C", call the clear function
+  if (selectedOperator === "C") return clear(); // return clear stops the function from running to prevent further errors
 }
 
 function evaluate() {
